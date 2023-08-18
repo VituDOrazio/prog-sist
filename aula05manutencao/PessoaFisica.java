5a4c4252b5a55abe0a02936b16494e879680dfdf
vitu <bortvitor40@gmail.com>
2023-08-18 10:33:41 -0300

    correção privacidade métodos classe PessoaFisica

--- aula05manutencao/PessoaFisica.java
+++ aula05manutencao/PessoaFisica.java
@@ -3,11 +3,11 @@ package aula05manutencao;
 public class PessoaFisica extends Pessoa {
     private String cpf;
 
-    protected String getCpf() {
+    public String getCpf() {
         return cpf;
     }
-    protected void setCpf(String cpf) {
+    public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-    
+
 }
